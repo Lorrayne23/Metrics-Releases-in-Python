e85c2a8bdf9f16d2a9fd9bb83faaca4848c92d02
Andre Murilo <35697459+andre-murilo@users.noreply.github.com>
2022-05-12 23:09:58 -0300

    Metrics

--- Codigo/get_releases.py
+++ Codigo/get_releases.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from datetime import date
 import tarfile
 
-token = 'place_token_here'
+token = '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'
 
 headers = {"Authorization": f"Bearer {token}"}
 
@@ -20,16 +20,10 @@ query = """{
       totalCount
       nodes {
         url
-        releaseAssets(first: 30) {
-          totalCount
-          nodes {
-            name
-            downloadUrl
-            downloadCount
-          }
-        }
+        tagName
       }
     }
+    url
   }
 }
 """
@@ -44,6 +38,7 @@ def run_query(query):
       else:
         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
     except:
+      print("Failed to query!")
       time.sleep(2)
       continue
 
@@ -59,15 +54,15 @@ def check_releases_extension(results):
             break
   return releases
 
-def save_releases(repo_owner, repo_name, releases):
+def save_releases(repo_owner, repo_name, url, releases):
   data = []
   name = repo_owner + '/' + repo_name
-  data.append([name, releases[0], releases[1], releases[2], releases[3], releases[4], releases[5], releases[6], releases[7], releases[8], releases[9]])
+  data.append([name, url, releases[0], releases[1], releases[2], releases[3], releases[4], releases[5], releases[6], releases[7], releases[8], releases[9]])
   df = pd.DataFrame(data) 
   df.to_csv("repo_releases.csv", mode='a', header=False, index=False)
 
 # Create file to save the data
-columns = ["Name", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"]
+columns = ["Name", "URL", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"]
 df = pd.DataFrame([], columns=columns) 
 df.to_csv("repo_releases.csv")
 with open('repositorios_populares_python.csv', newline='') as csvfile:
@@ -88,32 +83,37 @@ with open('repositorios_populares_python.csv', newline='') as csvfile:
       query = query.replace('"' + next_page + '"', 'null')
     old_repo_name = repo_name
     old_repo_owner = repo_owner
+    print(f'Repository: {repo_name}')
     query_result = run_query(query)
     results = query_result['data']['repository']['releases']['nodes']
+    url = query_result['data']['repository']['url']
     
     releases = []
-    releases = check_releases_extension(results)
+    for r in results:
+      releases.append(r["tagName"])
+
+    # releases = check_releases_extension(results)
 
     if len(releases) >= 10:
-      save_releases(repo_owner, repo_name, releases[0:10])
+      save_releases(repo_owner, repo_name, url, releases[0:10])
       continue
 
-    has_next_page = query_result['data']['repository']['releases']['pageInfo']['hasNextPage']
-    if has_next_page:
-      next_page = query_result['data']['repository']['releases']['pageInfo']['endCursor']
-      query = query.replace('null', '"' + next_page + '"')
+    # has_next_page = query_result['data']['repository']['releases']['pageInfo']['hasNextPage']
+    # if has_next_page:
+    #   next_page = query_result['data']['repository']['releases']['pageInfo']['endCursor']
+    #   query = query.replace('null', '"' + next_page + '"')
 
-    while has_next_page and len(releases) < 10:
-      old_pagination = query_result['data']['repository']['releases']['pageInfo']['endCursor']
-      query_result = run_query(query)
+    # while has_next_page and len(releases) < 10:
+    #   old_pagination = query_result['data']['repository']['releases']['pageInfo']['endCursor']
+    #   query_result = run_query(query)
 
-      valid_releases = check_releases_extension(results)
-      releases = releases + valid_releases
-      next_page = query_result['data']['repository']['releases']['pageInfo']['endCursor']
-      query = query.replace(old_pagination, next_page)
-      has_next_page = query_result['data']['repository']['releases']['pageInfo']['hasNextPage']
+    #   valid_releases = check_releases_extension(results)
+    #   releases = releases + valid_releases
+    #   next_page = query_result['data']['repository']['releases']['pageInfo']['endCursor']
+    #   query = query.replace(old_pagination, next_page)
+    #   has_next_page = query_result['data']['repository']['releases']['pageInfo']['hasNextPage']
 
-      if len(releases) >= 10:
-        save_releases(repo_owner, repo_name, releases[0:10])
+    #   if len(releases) >= 10:
+    #     save_releases(repo_owner, repo_name, releases[0:10])
 
 
--- Codigo/run_analyse.py
+++ Codigo/run_analyse.py
@@ -4,12 +4,12 @@ import json
 import ast
 import os
 import shutil
+import random
+import threading
+import queue
 from subprocess import PIPE, Popen
 
 # pip3 install radon
-
-# read csv
-csv = pandas.read_csv("repo_releases.csv")
 
 # create dir
 PATH_RELEASES = "temp/releases-downloaded"
@@ -21,57 +21,86 @@ if not os.path.exists(PATH_METRICS):
     os.makedirs(PATH_METRICS)
 
 # Download release
-def download_release(file_name, url):
+def download_release(file_name, url, tag):
     try:
-        print(f"Downloading: {url} ---> {file_name}")
-        data = requests.get(url, stream=True)
-        with open(file_name, "wb") as file:
-            file.write(data.content)
+        print(f"Downloading: {url} | {tag} ---> {file_name}")
+        os.system(f"git clone -b {tag} --depth 1 {url} {file_name}")
         return True
     except Exception as e:
         print(str(e))
         return False
 
-# Uncompress file
-def uncompress_release(file_name, clean_up=True):
-    # support .zip, .tar, .gztar, .bztar, .xztar
-    shutil.unpack_archive(file_name, PATH_RELEASES)
-
-    # remove packed file.
-    if clean_up:
-        os.remove(file_name)
-
-    # return folder name
-    return os.listdir(PATH_RELEASES)[0]
-
 # Run radon 
-def exec_radon(target_folder, clean_up=True):
+def handle_exec_thread(cmd):
+    os.system(cmd)
+
+def exec_radon(target_folder, repo_name, clean_up=True):
     print(f"Getting metrics from: [{target_folder}]")
-    os.system(f"env\Scripts\\radon.exe cc {PATH_RELEASES}/{target_folder} --average --total-average -j > cc_metrics.json")
-    os.system(f"env\Scripts\\radon.exe raw {PATH_RELEASES}/{target_folder} -s -j > raw_metrics.json")
-    os.system(f"env\Scripts\\radon.exe hal  {PATH_RELEASES}/{target_folder} -j > hal_metrics.json")
+
+    # files
+    file_cc = f"cc_metrics_{repo_name}.json"
+    file_raw = f"raw_metrics_{repo_name}.json"
+    file_hal = f"hal_metrics_{repo_name}.json"
+    file_mi = f"mi_metrics_{repo_name}.json"
+
+    # cmds
+    cmd_cc = f"radon cc {target_folder} --average --total-average -j > {file_cc}"
+    cmd_raw = f"radon raw {target_folder} -s -j > {file_raw}"
+    cmd_hal = f"radon hal  {target_folder} -j > {file_hal}"
+    cmd_mi = f"radon mi  {target_folder} -j > {file_mi}"
+
+    # threads
+    thread_cc = threading.Thread(target=handle_exec_thread, args=(cmd_cc,))
+    thread_raw = threading.Thread(target=handle_exec_thread, args=(cmd_raw,))
+    thread_hal = threading.Thread(target=handle_exec_thread, args=(cmd_hal,))
+    thread_mi = threading.Thread(target=handle_exec_thread, args=(cmd_mi,))
+
+    # star threads
+    thread_cc.start()
+    thread_raw.start()
+    thread_hal.start()
+    thread_mi.start()
+
+    # wait threads
+    thread_cc.join()
+    thread_raw.join()
+    thread_hal.join()
+    thread_mi.join()
 
     # return cc_metrics.json
-    f = open("cc_metrics.json", "rt", encoding="utf-8")
+    f = open(file_cc, "rt", encoding="utf-8")
     cc_data = f.read()
     f.close()
 
     # return raw_metrics.json
-    f = open("raw_metrics.json", "rt", encoding="utf-8")
+    f = open(file_raw, "rt", encoding="utf-8")
     raw_data = f.read()
     f.close()
 
     # return hal_metrics.json
-    f = open("hal_metrics.json", "rt", encoding="utf-8")
+    f = open(file_hal, "rt", encoding="utf-8")
     hal_data = f.read()
     f.close()
 
+    # return mi_metrics.json
+    f = open(file_mi, "rt", encoding="utf-8")
+    mi_data = f.read()
+    f.close()
+
+
     # clean up
     if clean_up:
-        shutil.rmtree(f"{PATH_RELEASES}/{target_folder}", ignore_errors=True)
-        # os.remove("metrics.json")
-    # return json.loads([cc_data, raw_data, hal_data])
-    return [cc_data, raw_data, hal_data]
+        shutil.rmtree(target_folder, ignore_errors=True)
+
+        try:
+            os.remove(file_cc)
+            os.remove(file_hal)
+            os.remove(file_raw)
+            os.remove(file_mi)
+        except:
+            print("Failed to delete random metrics!")
+
+    return [cc_data, raw_data, hal_data, mi_data]
 
 # Save Metrics
 def save_metric(data):
@@ -81,46 +110,77 @@ def save_metric(data):
     f.write(json.dumps(data))
     f.close()
 
-
-
-# main
-for index, repository in csv.iterrows():
-    name = repository["Name"]
-    releases = []
-    releases.append(repository["R1"])
-    releases.append(repository["R2"])
-    releases.append(repository["R3"])
-    releases.append(repository["R4"])
-    releases.append(repository["R5"])
-    releases.append(repository["R6"])
-    releases.append(repository["R7"])
-    releases.append(repository["R8"])
-    releases.append(repository["R9"])
-    releases.append(repository["R10"])
-
-    data = {
-        "repo_name": name.split('/')[1],
-        "cc_metrics": [],
-        "raw_metrics": [],
-        "hal_metrics": [],
+def proc_analyseThread(queue):
+    while True:
+        repository = queue.get()
         
-    }
+        # repository vars
+        name = repository["Name"]
+        url = repository["URL"]
 
-    # check if metric is already collected ...
-    if not os.path.exists(f"{PATH_METRICS}/metrics_{data['repo_name']}.json"):
-        # download releases;
-        # uncompress;
-        # execute radon;
-        # add to metrics lists;
-        # save metrics;
-        for release in releases:
-            file_name = PATH_RELEASES + "/" + release.split('/')[-1]
-            if download_release(file_name, release):
-                target_folder = uncompress_release(file_name)
-                metric_json = []
-                metric_json = exec_radon(target_folder)
-                data["cc_metrics"].append(json.loads(metric_json[0]))
-                data["raw_metrics"].append(json.loads(metric_json[1]))
-                data["hal_metrics"].append(json.loads(metric_json[2]))
+        releases = []
+        releases.append(repository["R1"])
+        releases.append(repository["R2"])
+        releases.append(repository["R3"])
+        releases.append(repository["R4"])
+        releases.append(repository["R5"])
+        releases.append(repository["R6"])
+        releases.append(repository["R7"])
+        releases.append(repository["R8"])
+        releases.append(repository["R9"])
+        releases.append(repository["R10"])
 
-        save_metric(data)
+        data = {
+            "repo_name": name.split('/')[1],
+            "cc_metrics": [],
+            "raw_metrics": [],
+            "hal_metrics": [],
+            "mi_metrics": []
+        }
+
+        # check if metric is already collected ...
+        if not os.path.exists(f"{PATH_METRICS}/metrics_{data['repo_name']}.json"):
+            # download releases;
+            # uncompress;
+            # execute radon;
+            # add to metrics lists;
+            # save metrics;
+            for release in releases:
+                file_name = PATH_RELEASES + "/" + release.split('/')[-1]
+                
+                # check if path already exists
+                if os.path.exists(file_name):
+                    rand1 = str(random.randint(0,999999))
+                    rand2 = str(random.randint(100000,999999))
+                    file_name += '_' + rand1 + "_" + rand2
+
+                if download_release(file_name, url, release):
+                    # target_folder = uncompress_release(file_name)
+                    metric_json = []
+                    metric_json = exec_radon(file_name, data["repo_name"])
+                    data["cc_metrics"].append(json.loads(metric_json[0]))
+                    data["raw_metrics"].append(json.loads(metric_json[1]))
+                    data["hal_metrics"].append(json.loads(metric_json[2]))
+                    data["mi_metrics"].append(json.loads(metric_json[3]))
+
+            save_metric(data)
+        queue.task_done()
+
+# SETUP QUEUE
+THREAD_COUNT = 20
+poll_queue = queue.Queue()
+print("[*] Thread Count:", THREAD_COUNT)
+for i in range(THREAD_COUNT):
+    try:
+        worker = threading.Thread(target=proc_analyseThread, args=(poll_queue,))
+        worker.start()
+    except Exception as e:
+        print(str(e))
+
+# PUT IN QUEUE
+csv = pandas.read_csv("repo_releases.csv")
+for index, repository in csv.iterrows():
+    poll_queue.put(repository)
+
+# Wait Poll Threads Done!
+poll_queue.join()
